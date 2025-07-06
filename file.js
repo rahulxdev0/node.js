7e38bc98448caee5c3181680f538200bf10708bf
rahulxdev0 <rk854327@gmail.com>
2025-07-06 12:20:02 +0530

    learning fs module

---
+++ file.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const data = fs.writeFileSync('test.txt', 'Hello, World!');
+
+
+// Asynchronous read
+fs.readFile('test.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+    console.log('Asynchronous read:', data);
+});
+
+
+// Synchronous read
+const fileContent = fs.readFileSync('test.txt', 'utf8');
+console.log('File content:', fileContent);
+
+const value = fs.appendFileSync('test.txt', '\nAppended text.');
+console.log('File after append:', value);
