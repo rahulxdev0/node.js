6e688b6dc1e04853e1d60c511692bac532e82224
rahulxdev0 <rk854327@gmail.com>
2025-07-06 12:28:23 +0530

    addinf file directory using mkdirSync

--- file.js
+++ file.js
@@ -19,3 +19,15 @@ console.log('File content:', fileContent);
 
 const value = fs.appendFileSync('test.txt', '\nAppended text.');
 console.log('File after append:', value);
+
+fs.mkdirSync('newDir', { recursive: true });
+console.log('Directory created successfully.');
+
+fs.readdir('newDir', (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+    console.log('Files in newDir:', files);
+});
+
