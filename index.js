d0820d5168444b5c9ecad1a925c364c2d5a3c8bc
rahulxdev0 <rk854327@gmail.com>
2025-07-10 13:12:58 +0530

    adding query search funcationality using url pakage

--- index.js
+++ index.js
@@ -6,15 +6,23 @@
 
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
+    if(req.url === "/favicon.ico") return res.end();
     const log = `${Date.now().toLocaleString()} : ${req.url} New Request Received\n`;
+    const myUrl = url.parse(req.url, true);
     fs.appendFile("log.txt", log, (err, data) =>{
-       switch(req.url){
+       switch(myUrl.pathname){
         case "/" : res.end("Home page");
         break;
-        case "/about" : res.end("this is about page");
-        break;
+        case "/about" : //about?myname=Rahul Kumar
+            const username = myUrl.query.myname;
+            res.end(`Hi, ${username}`);
+            break;
+        case "/search" : 
+            const search = myUrl.query.search_query;
+            res.end("here is your result for " + search);
         default:
             res.end("404 Not Found");
        }
