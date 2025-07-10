4ddc706a030d8c4aadb20d521312e3f6fc4664d4
rahulxdev0 <rk854327@gmail.com>
2025-07-10 12:56:01 +0530

    creating server in node js and log the req url in log file

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const {greet, greetWithName} = require('./hello.js');
+// const {greet, greetWithName} = require('./hello.js');
 
-greet();
-greetWithName("Alice"); 
+// greet();
+// greetWithName("Alice"); 
+
+
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now().toLocaleString()} : ${req.url} New Request Received\n`;
+    fs.appendFile("log.txt", log, (err, data) =>{
+       switch(req.url){
+        case "/" : res.end("Home page");
+        break;
+        case "/about" : res.end("this is about page");
+        break;
+        default:
+            res.end("404 Not Found");
+       }
+        
+    })
+});
+
+myServer.listen(8000, () => console.log("server started!"));
